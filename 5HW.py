47ece7e31f6e3488f54c772cc4db3c0d9200c2cb
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-07-26 12:33:15 +0100

    Add files via upload

---
+++ 5HW.py
@@ -0,0 +1,6 @@
+Temperature = int(input("What is the temperature today? "))
+
+if Temperature >= 20:
+    print("You should wear light, easy and comfortable clothes.")
+else:
+    print("You should wear warm clothes and a jacket.")
